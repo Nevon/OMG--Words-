9b74d5a3055bc2ed65861acb983b498e926de8a4
bartbes <bart.bes+projects@gmail.com>
2010-08-13 12:08:51 +0200

    Added falling back to lastpost.txt if it can't download the post

--- downloading.lua
+++ downloading.lua
@@ -11,28 +11,28 @@ function state:draw()
 	--Shadows for the side of the main area
 	love.graphics.draw(images.sideshadow, 37, 132)
 	love.graphics.draw(images.sideshadow, 762, 132, 0, -1, 1)
-	
+
 	--Draw the main white area
 	love.graphics.rectangle("fill", 50, 140, 700, 400)
-	
+
 	--The head banner
 	love.graphics.draw(images.banner, 0, 10)
-	
+
 	--head
 	love.graphics.draw(images.tommy, 165, 287)
 	--bubble
 	love.graphics.draw(images.bubbles.downloading, 222, 287)
-	
+
 	love.graphics.setColor(66,66,66)
 	love.graphics.setFont(fonts.bold12)
-	
+
 	love.graphics.printf("Press                to download the words of the almighty. If you don't have an internet connection, this WILL fail.", 252, 315, 360, "left")
 	love.graphics.print("Press          to quit", 252, 360)
-	
+
 	love.graphics.setColor(241,93,34)
 	love.graphics.print("ENTER", 289, 315)
 	love.graphics.print("ESC", 289, 360)
-	
+
 	--Footer
 	love.graphics.setColor(42,44,46)
 	love.graphics.rectangle("fill", 0, 530, 800, 70)
@@ -53,13 +53,23 @@ end
 database = {}
 function downloadWords()
 	local http = require("socket.http")
-	local a = http.request("http://omgubuntu.co.uk/")
-	local b = http.request(a:match("<h3 class='post%-title entry%-title'>\n<a href='([^']+)"))
-	local post = b:match("<div class='post%-body entry%-content'>(.+)</div>\n<div class='post%-footer'"):gsub("&nbsp;", " "):gsub("<a[^>]+>[^<]+</a>", ""):gsub("<[^>]+>", "")
-	
-	post = post:gsub("[^%a]", " ")
-	
+	http.TIMEOUT = 5
+	local success, post = pcall(function ()
+		local a = http.request("http://omgubuntu.co.uk/")
+		local b = http.request(a:match("<h3 class='post%-title entry%-title'>\n<a href='([^']+)"))
+		return b:match("<div class='post%-body entry%-content'>(.+)</div>\n<div class='post%-footer'"):gsub("&nbsp;", " "):gsub("<a[^>]+>[^<]+</a>", ""):gsub("<[^>]+>", "")
+	end)
+
+	if not success then
+		print("Failed, falling back to last downloaded post.")
+		post = love.filesystem.read("lastpost.txt")
+	else
+		print("Success.")
+		post = post:gsub("[^%a]", " ")
+		love.filesystem.write("lastpost.txt", post)
+	end
+
 	database = Database(post)
-	print ("Done\!")
+	print ("Post read\!")
 	Gamestate.switch(Gamestate.game)
 end
--- main.lua
+++ main.lua
@@ -12,21 +12,23 @@ require("explosion")
 
 function love.load()
 	love.graphics.setBackgroundColor(247, 246, 245)
-	
+
+	love.filesystem.setIdentity("omgwords")
+
 	--get us a randomseed before generating random data
 	math.randomseed(love.timer.getMicroTime()*1000000)
-	
+
 	--load the images
 	images = {}
 	loadfromdir(images, "gfx", "png", love.graphics.newImage)
 	--load the sound (effects)
 	sounds = {}
 	loadfromdir(sounds, "snd/sfx", "ogg", love.sound.newSoundData)
-	
+
 	--and the music
 	music = {}
 	loadfromdir(music, "snd/music", "ogg", love.audio.newSource)
-	
+
 	--load fonts
 	fonts = {
 		bold12 = love.graphics.newFont("gfx/fonts/NimbusSansL-Bold.ttf", 12),
@@ -36,7 +38,7 @@ function love.load()
 		bold24 = love.graphics.newFont("gfx/fonts/NimbusSansL-Bold.ttf", 24),
 		mono28 = love.graphics.newFont("gfx/fonts/NimbusMonoL-Bold.ttf", 28)
 	}
-	
+
 	Gamestate.registerEvents()
 	Gamestate.switch(Gamestate.title)
 end
